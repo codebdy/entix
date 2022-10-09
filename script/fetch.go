83e27a7e944119f7965c82458a6fed9ff0c4534a
Water.Li <lizongbin@hotmail.com>
2022-10-09 17:01:57 +0800

    Update fetch.go

--- script/fetch.go
+++ script/fetch.go
@@ -8,20 +8,20 @@ import (
 	"github.com/dop251/goja_nodejs/eventloop"
 )
 
-type FetchFn func() *goja.Promise
+type FetchFn func(url string, options map[string]interface{}) *goja.Promise
 
 func GetFetchFn(vm *goja.Runtime) FetchFn {
-	return func() *goja.Promise {
+	return func(url string, options map[string]interface{}) *goja.Promise {
 		loop := eventloop.NewEventLoop()
 		loop.Start()
 		defer loop.Stop()
-		fmt.Println("创建Promise")
+		fmt.Println("执行Promise,接收的参数", url, options)
 		p, resolve, _ := vm.NewPromise()
 		loop.RunOnLoop(func(vm *goja.Runtime) {
 			go func() {
 				time.Sleep(500 * time.Millisecond) // or perform any other blocking operation
 				fmt.Println("等待Promise结束")
-				resolve("result")
+				resolve("golang返回的的Fetch 结果")
 			}()
 		})
 		return p
