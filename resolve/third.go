004c4d10ccdde61179d3e73d842d936f1060111b
Water.Li <lizongbin@hotmail.com>
2022-10-10 17:53:52 +0800

    Update third.go

--- resolve/third.go
+++ resolve/third.go
@@ -1,17 +1,39 @@
 package resolve
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/dop251/goja"
+	"github.com/dop251/goja_nodejs/eventloop"
 	"github.com/graphql-go/graphql"
 	"rxdrag.com/entify/model"
 	"rxdrag.com/entify/model/graph"
+	"rxdrag.com/entify/script"
 	"rxdrag.com/entify/utils"
 )
 
 func QueryThirdPartyResolveFn(third *graph.ThirdParty, model *model.Model) graphql.FieldResolveFn {
 	return func(p graphql.ResolveParams) (interface{}, error) {
 		defer utils.PrintErrorStack()
+		if strings.Trim(third.Domain.QueryScript, " ") == "" {
+			return nil, nil
+		}
+		vm := goja.New()
+		script.Enable(vm)
+		loop := eventloop.NewEventLoop()
+		loop.Start()
+		defer loop.Stop()
 
-		return nil, nil
+		wait := make(chan string, 1)
+		vm.Set("callback", func(call goja.FunctionCall) goja.Value {
+			fmt.Println("Go收到返回值")
+			wait <- call.Argument(0).ToString().String()
+			return nil
+		})
+		vm.RunString(third.Domain.QueryScript)
+
+		return <-wait, nil
 	}
 }
 
