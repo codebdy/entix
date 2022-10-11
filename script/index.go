366ed0ea7f706f92930eb63c7b48d96419ddf61d
Water.Li <lizongbin@hotmail.com>
2022-10-11 10:17:05 +0800

    Update index.go

--- script/index.go
+++ script/index.go
@@ -3,7 +3,7 @@ package script
 import "github.com/dop251/goja"
 
 func Enable(vm *goja.Runtime) {
-	vm.Set("goFetch", GoFetchFn)
+	vm.Set("iFetch", GoFetchFn)
 	vm.Set("writeToCache", WriteToCache)
 	vm.Set("readFromCache", ReadFromCache)
 }
