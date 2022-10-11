a2b82692b73d59e22fd8085f9e9742a0e66d6cba
Water.Li <lizongbin@hotmail.com>
2022-10-11 09:42:31 +0800

    backup

--- script/fetch.go
+++ script/fetch.go
@@ -5,55 +5,45 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-
-	"github.com/dop251/goja"
-	"github.com/dop251/goja_nodejs/eventloop"
 )
 
-type GoFetchFn func(url string, options map[string]interface{}) string
+func GoFetchFn(url string, options map[string]interface{}) string {
+	method := http.MethodGet
+	if options != nil && options["method"] != nil {
+		method = options["method"].(string)
+	}
 
-func GetFetchFn(vm *goja.Runtime) GoFetchFn {
-	return func(url string, options map[string]interface{}) string {
-		loop := eventloop.NewEventLoop()
-		loop.Start()
-		defer loop.Stop()
-		method := http.MethodGet
-		if options != nil && options["method"] != nil {
-			method = options["method"].(string)
-		}
+	reqBody := []byte("")
+	if options != nil && options["body"] != nil {
+		reqBody = []byte(options["body"].(string))
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, bytes.NewBuffer(reqBody))
 
-		reqBody := []byte("")
-		if options != nil && options["body"] != nil {
-			reqBody = []byte(options["body"].(string))
-		}
-		client := &http.Client{}
-		req, err := http.NewRequest(method, url, bytes.NewBuffer(reqBody))
-
-		if err != nil {
-			fmt.Println(err)
-			return ""
-		}
-		if options != nil && options["headers"] != nil {
-			headers := options["headers"].(map[string]interface{})
-			for key, header := range headers {
-				if header != nil {
-					req.Header.Add(key, header.(string))
-				}
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	if options != nil && options["headers"] != nil {
+		headers := options["headers"].(map[string]interface{})
+		for key, header := range headers {
+			if header != nil {
+				req.Header.Add(key, header.(string))
 			}
 		}
-
-		res, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return ""
-		}
-		defer res.Body.Close()
-
-		body, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println(err)
-			return ""
-		}
-		return string(body)
 	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(body)
 }
