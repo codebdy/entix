b26ef1fc81336266d1b5e29b72b27629f7a5d68c
Water.Li <lizongbin@hotmail.com>
2022-10-20 12:24:57 +0800

    backup

--- entry/modules.go
+++ entry/modules.go
@@ -2,18 +2,75 @@ package entry
 
 import (
 	"context"
+	"log"
 
 	"github.com/graphql-go/graphql"
+)
+
+const (
+	ROOT_QUERY_NAME        = "Query"
+	ROOT_MUTATION_NAME     = "Mutation"
+	ROOT_SUBSCRIPTION_NAME = "Subscription"
 )
 
 var modules []Moduler = []Moduler{}
 
 type Moduler interface {
-	QueryFields(ctx context.Context) []*graphql.Field
-	MutationFields(ctx context.Context) []*graphql.Field
-	SubscriptionFields(ctx context.Context) []*graphql.Field
+	QueryFields(ctx context.Context) []graphql.Field
+	MutationFields(ctx context.Context) []graphql.Field
+	SubscriptionFields(ctx context.Context) []graphql.Field
+	Directives(ctx context.Context) []*graphql.Directive
+	Types(ctx context.Context) []graphql.Type
 }
 
-func GetSchema() *graphql.Schema {
+func GetSchema(ctx context.Context) graphql.Schema {
+	rootQueryFields := graphql.Fields{}
+	rootMutationFields := graphql.Fields{}
+	rootSubscriptionFields := graphql.Fields{}
+	directives := []*graphql.Directive{}
+	types := []graphql.Type{}
 
+	for _, module := range modules {
+		for _, field := range module.QueryFields(ctx) {
+			rootQueryFields[field.Name] = &field
+		}
+		for _, field := range module.MutationFields(ctx) {
+			rootMutationFields[field.Name] = &field
+		}
+		for _, field := range module.SubscriptionFields(ctx) {
+			rootSubscriptionFields[field.Name] = &field
+		}
+		directives = append(directives, module.Directives(ctx)...)
+		types = append(types, module.Types(ctx)...)
+	}
+
+	rootQuery := graphql.NewObject(graphql.ObjectConfig{
+		Name:   ROOT_QUERY_NAME,
+		Fields: rootQueryFields,
+	})
+
+	rootMutation := graphql.NewObject(graphql.ObjectConfig{
+		Name:   ROOT_MUTATION_NAME,
+		Fields: rootMutationFields,
+	})
+
+	rootSubscription := graphql.NewObject(graphql.ObjectConfig{
+		Name:   ROOT_SUBSCRIPTION_NAME,
+		Fields: rootSubscriptionFields,
+	})
+
+	schemaConfig := graphql.SchemaConfig{
+		Query:        rootQuery,
+		Mutation:     rootMutation,
+		Subscription: rootSubscription,
+		Directives:   directives,
+		Types:        types,
+	}
+	schema, err := graphql.NewSchema(schemaConfig)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return schema
 }
