e8b2bceef2fd4db4dd1b012cc3ccbb24aa3f58ff
Water.Li <lizongbin@hotmail.com>
2022-10-20 12:44:49 +0800

    Create module.go

---
+++ install/module.go
@@ -0,0 +1,35 @@
+package install
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/graphql-go/graphql"
+	"rxdrag.com/entify/entry"
+)
+
+type InstalModule struct {
+}
+
+func (m InstalModule) QueryFields(ctx context.Context) []graphql.Field {
+	return []graphql.Field{}
+}
+func (m InstalModule) MutationFields(ctx context.Context) []graphql.Field {
+	return []graphql.Field{}
+}
+func (m InstalModule) SubscriptionFields(ctx context.Context) []graphql.Field {
+	return []graphql.Field{}
+}
+func (m InstalModule) Directives(ctx context.Context) []*graphql.Directive {
+	return []*graphql.Directive{}
+}
+func (m InstalModule) Types(ctx context.Context) []graphql.Type {
+	return []graphql.Type{}
+}
+func (m InstalModule) Middlewares() []func(next http.Handler) http.Handler {
+	return []func(next http.Handler) http.Handler{}
+}
+
+func init() {
+	entry.AddModuler(InstalModule{})
+}
