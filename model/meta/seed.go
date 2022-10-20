738824d2f6a2c6e79ad49fc89cec8fb514a7c0b5
Water.Li <lizongbin@hotmail.com>
2022-10-20 09:48:18 +0800

    backup

--- model/meta/seed.go
+++ model/meta/seed.go
@@ -1,5 +1,7 @@
 package meta
 
+import "rxdrag.com/entify/consts"
+
 var SystemApp = map[string]interface{}{
 	"uuid": SYSTEM_APP_UUID,
 	"name": "Apper",
@@ -17,5 +19,219 @@ var SystemApp = map[string]interface{}{
 			RoleClass,
 		},
 		Relations: Relations,
+	},
+}
+
+var AppClass = ClassMeta{
+	Uuid:       APP_ENTITY_UUID,
+	Name:       APP_ENTITY_NAME,
+	InnerId:    APP_INNER_ID,
+	StereoType: CLASSS_ENTITY,
+	IdNoShift:  true,
+	Root:       true,
+	System:     true,
+	Attributes: []AttributeMeta{
+		{
+			Name:      consts.ID,
+			Primary:   true,
+			Type:      ID,
+			TypeLabel: ID,
+			Uuid:      "APP_COLUMN_ID_UUID",
+			System:    true,
+		},
+		{
+			Name:      "uuid",
+			Type:      STRING,
+			TypeLabel: STRING,
+			Uuid:      "APP_COLUMN_UUID_UUID",
+			System:    true,
+		},
+		{
+			Name:      "name",
+			Type:      STRING,
+			TypeLabel: STRING,
+			Uuid:      "APP_COLUMN_NAME_UUID",
+			System:    true,
+		},
+		{
+			Name:      "meta",
+			Type:      JSON,
+			TypeLabel: JSON,
+			Uuid:      "APP_COLUMN_META_UUID",
+			System:    true,
+		},
+		{
+			Name:      "publishedMeta",
+			Type:      JSON,
+			TypeLabel: JSON,
+			Uuid:      "APP_COLUMN_PUBLISH_META_UUID",
+			System:    true,
+		},
+		{
+			Name:       "createdAt",
+			Type:       DATE,
+			TypeLabel:  DATE,
+			CreateDate: true,
+			Uuid:       "APP_COLUMN_CREATED_AT_UUID",
+			System:     true,
+		},
+		{
+			Name:       "saveMetaAt",
+			Type:       DATE,
+			TypeLabel:  DATE,
+			CreateDate: true,
+			Uuid:       "APP_COLUMN_SAVE_META_AT_UUID",
+			System:     true,
+		},
+		{
+			Name:       "publishMetaAt",
+			Type:       DATE,
+			TypeLabel:  DATE,
+			CreateDate: true,
+			Uuid:       "APP_COLUMN_PUBLISH_META_AT_UUID",
+			System:     true,
+		},
+	},
+	PackageUuid: PACKAGE_SYSTEM_UUID,
+}
+
+var UserClass = ClassMeta{
+	PackageUuid: PACKAGE_SYSTEM_UUID,
+	InnerId:     USER_INNER_ID,
+	Name:        USER_ENTITY_NAME,
+	Root:        true,
+	StereoType:  CLASSS_ENTITY,
+	Uuid:        USER_ENTITY_UUID,
+	System:      true,
+	Attributes: []AttributeMeta{
+		{
+			System:    true,
+			Name:      consts.ID,
+			Primary:   true,
+			Type:      ID,
+			TypeLabel: ID,
+			Uuid:      "RX_USER_ID_UUID",
+		},
+		{
+			System:    true,
+			Name:      "name",
+			Nullable:  true,
+			Type:      STRING,
+			TypeLabel: STRING,
+			Uuid:      "RX_USER_NAME_UUID",
+		},
+		{
+			System:    true,
+			Length:    128,
+			Name:      "loginName",
+			Type:      STRING,
+			TypeLabel: STRING,
+			Uuid:      "RX_USER_LOGINNAME_UUID",
+		},
+		{
+			System:    true,
+			Length:    256,
+			Name:      "password",
+			Type:      STRING,
+			TypeLabel: STRING,
+			Uuid:      "RX_USER_PASSWORD_UUID",
+		},
+		{
+			System:    true,
+			Name:      "isSupper",
+			Nullable:  true,
+			Type:      BOOLEAN,
+			TypeLabel: BOOLEAN,
+			Uuid:      "RX_USER_ISSUPPER_UUID",
+		},
+		{
+			System:    true,
+			Name:      "isDemo",
+			Nullable:  true,
+			Type:      BOOLEAN,
+			TypeLabel: BOOLEAN,
+			Uuid:      "RX_USER_ISDEMO_UUID",
+		},
+		{
+			System:     true,
+			CreateDate: true,
+			Name:       "createdAt",
+			Type:       DATE,
+			TypeLabel:  DATE,
+			Uuid:       "RX_USER_CREATEDAT_UUID",
+		},
+		{
+			System:     true,
+			Name:       "updatedAt",
+			Type:       DATE,
+			TypeLabel:  DATE,
+			UpdateDate: true,
+			Uuid:       "RX_USER_UPDATEDAT_UUID",
+		},
+	},
+}
+
+var RoleClass = ClassMeta{
+	PackageUuid: PACKAGE_SYSTEM_UUID,
+	InnerId:     ROLE_INNER_ID,
+	Name:        ROLE_ENTITY_NAME,
+	Root:        true,
+	StereoType:  CLASSS_ENTITY,
+	Uuid:        ROLE_ENTITY_UUID,
+	System:      true,
+	Attributes: []AttributeMeta{
+		{
+			System:    true,
+			Name:      consts.ID,
+			Primary:   true,
+			Type:      ID,
+			TypeLabel: ID,
+			Uuid:      "RX_ROLE_ID_UUID",
+		},
+		{
+			System:    true,
+			Name:      "name",
+			Type:      "String",
+			TypeLabel: "String",
+			Uuid:      "RX_ROLE_NAME_UUID",
+		},
+		{
+			System:    true,
+			Name:      "description",
+			Nullable:  true,
+			Type:      "String",
+			TypeLabel: "String",
+			Uuid:      "RX_ROLE_DESCRIPTION_UUID",
+		},
+		{
+			System:     true,
+			CreateDate: true,
+			Name:       "createdAt",
+			Type:       DATE,
+			TypeLabel:  DATE,
+			Uuid:       "RX_ROLE_CREATEDAT_UUID",
+		},
+		{
+			System:     true,
+			Name:       "updatedAt",
+			Type:       DATE,
+			TypeLabel:  DATE,
+			UpdateDate: true,
+			Uuid:       "RX_ROLE_META_UPDATEDAT_UUID",
+		},
+	},
+}
+
+var Relations = []RelationMeta{
+	{
+		InnerId:            ROLE_USER_RELATION_INNER_ID,
+		RelationType:       "twoWayAssociation",
+		RoleOfSource:       "roles",
+		RoleOfTarget:       "users",
+		SourceId:           ROLE_ENTITY_UUID,
+		SourceMutiplicity:  "0..*",
+		TargetId:           USER_ENTITY_UUID,
+		TargetMultiplicity: "0..*",
+		Uuid:               "META_RELATION_USER_ROLE_UUID",
 	},
 }
