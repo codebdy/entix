52dc9e0da75ddc103e5aa04ef13d2128881948ea
Water.Li <lizongbin@hotmail.com>
2022-10-22 18:03:59 +0800

    backup

--- app/publish.go
+++ app/publish.go
@@ -1,9 +1,14 @@
 package app
 
 import (
+	"log"
+	"time"
+
 	"rxdrag.com/entify/model"
+	"rxdrag.com/entify/model/data"
 	"rxdrag.com/entify/model/meta"
 	"rxdrag.com/entify/orm"
+	"rxdrag.com/entify/service"
 )
 
 func PublishMeta(published, next *meta.MetaContent, appId uint64) {
@@ -13,7 +18,54 @@ func PublishMeta(published, next *meta.MetaContent, appId uint64) {
 	orm.Migrage(diff)
 }
 
-func (a *App) Publish() error {
-	//需要MergeModel
-	return nil
+func (a *App) Publish() {
+	entity := a.GetEntityByName(meta.APP_ENTITY_NAME)
+
+	appData := service.QueryById(
+		entity,
+		a.AppId,
+	)
+
+	appMap := appData.(map[string]interface{})
+
+	nextMeta := appMap["meta"].(meta.MetaContent)
+	oldMeta := appMap["publishedMeta"].(meta.MetaContent)
+	PublishMeta(a.MergeModel(&oldMeta), a.MergeModel(&nextMeta), a.AppId)
+
+	appMap["publishedMeta"] = appMap["meta"]
+	appMap["publishMetaAt"] = time.Now()
+	instance := data.NewInstance(
+		appMap,
+		entity,
+	)
+
+	_, err := service.SaveOne(instance)
+
+	if err != nil {
+		log.Panic(err.Error())
+	}
+
+	a.ReLoad()
+}
+
+func (a *App) MergeModel(content *meta.MetaContent) *meta.MetaContent {
+	//合并系统Schema
+	if a.AppId != meta.SYSTEM_APP_ID {
+		systemAppData := service.QueryById(
+			a.GetEntityByName(meta.APP_ENTITY_NAME),
+			meta.SYSTEM_APP_ID,
+		)
+
+		systemContent := systemAppData.(map[string]interface{})["publishedMeta"].(meta.MetaContent)
+		//systemMetaContent := r.LoadAndDecodeMeta(consts.SYSTEM_APP_UUID)
+		for i := range systemContent.Classes {
+			content.Classes = append(content.Classes, systemContent.Classes[i])
+		}
+
+		for i := range systemContent.Relations {
+			content.Relations = append(content.Relations, systemContent.Relations[i])
+		}
+	}
+
+	return content
 }
--- publish/publish.go
+++ publish/publish.go
@@ -1,15 +1,9 @@
 package publish
 
 import (
-	"log"
-	"time"
-
 	"github.com/graphql-go/graphql"
 	"rxdrag.com/entify/app"
 	"rxdrag.com/entify/logs"
-	"rxdrag.com/entify/model/data"
-	"rxdrag.com/entify/model/meta"
-	"rxdrag.com/entify/service"
 	"rxdrag.com/entify/utils"
 )
 
@@ -23,28 +17,5 @@ func PublishMetaResolveFn(app *app.App) graphql.FieldResolveFn {
 }
 
 func doPublish(app *app.App) {
-	entity := app.GetEntityByName(meta.APP_ENTITY_NAME)
-	appData := service.QueryById(
-		entity,
-		app.AppId,
-	)
-	if app == nil {
-		log.Panic("App is nil")
-	}
-
-	appMap := appData.(map[string]interface{})
-	appMap["publishedMeta"] = appMap["meta"]
-	appMap["publishMetaAt"] = time.Now()
-	instance := data.NewInstance(
-		appMap,
-		entity,
-	)
-
-	_, err := service.SaveOne(instance)
-
-	if err != nil {
-		log.Panic(err.Error())
-	}
-
-	app.ReLoad()
+	app.Publish()
 }
