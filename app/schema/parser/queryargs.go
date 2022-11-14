9c1cdcf5d2da8315a05732e030099df315d434bc
Water.Li <lizongbin@hotmail.com>
2022-11-14 09:49:09 +0800

    Update queryargs.go

--- app/schema/parser/queryargs.go
+++ app/schema/parser/queryargs.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"log"
+
 	"github.com/graphql-go/graphql"
 	"rxdrag.com/entify/consts"
 	"rxdrag.com/entify/model/graph"
@@ -184,9 +186,33 @@ func (p *ModelParser) MethodArgs(methodArgs []meta.ArgMeta) graphql.FieldConfigA
 
 	for _, arg := range methodArgs {
 		args[arg.Name] = &graphql.ArgumentConfig{
-			Type: PropertyType(arg.Type),
+			Type: p.argType(arg),
 		}
 	}
 
 	return args
+}
+
+func (p *ModelParser) argType(arg meta.ArgMeta) graphql.Output {
+	switch arg.Type {
+	case meta.ENTITY:
+		entity := p.model.Graph.GetEntityByUuid(arg.TypeUuid)
+		if entity == nil {
+			log.Panic("Can not find entity by uuid:" + arg.TypeUuid)
+		}
+		return p.SaveInput(entity.Name())
+	case meta.ENTITY_ARRAY:
+		entity := p.model.Graph.GetEntityByUuid(arg.TypeUuid)
+		if entity == nil {
+			log.Panic("Can not find entity by uuid:" + arg.TypeUuid)
+		}
+		return &graphql.NonNull{
+			OfType: &graphql.List{
+				OfType: &graphql.NonNull{
+					OfType: p.SaveInput(entity.Name()),
+				},
+			},
+		}
+	}
+	return PropertyType(arg.Type)
 }
