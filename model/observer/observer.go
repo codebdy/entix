54c32af39308147258ceb90c5fd4654f985fdc5a
Water.Li <lizongbin@hotmail.com>
2022-11-15 11:45:09 +0800

    Create observer.go

---
+++ model/observer/observer.go
@@ -0,0 +1,20 @@
+package observer
+
+import "sync"
+
+type ModelObserver interface {
+	Key() string
+	ObjectCreated(object map[string]interface{})
+	ObjectUpdated(object map[string]interface{})
+	OjbectDeleted(object map[string]interface{})
+}
+
+var ModelObservers sync.Map
+
+func AddObserver(obsr ModelObserver) {
+	ModelObservers.Store(obsr.Key(), obsr)
+}
+
+func RemoveObserver(key string) {
+	ModelObservers.Delete(key)
+}
