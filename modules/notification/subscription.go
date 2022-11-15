70924d663d97be04abc0614f2e02d7fc1695df89
Water.Li <lizongbin@hotmail.com>
2022-11-15 16:55:29 +0800

    Update subscription.go

--- modules/notification/subscription.go
+++ modules/notification/subscription.go
@@ -4,10 +4,6 @@ import (
 	"github.com/graphql-go/graphql"
 )
 
-//1、无限通道存储事件源 ==== 不用无限通道，单独开一个线程发送消息
-//2、单独线程分发事件
-//3、所有订阅列表
-
 func (m *SubscriptionModule) SubscriptionFields() []*graphql.Field {
 	if m.app != nil {
 		return []*graphql.Field{
